8101acdb26e45a53428ba8ef12fe42d142ba59df
riyaz code <ra5311026@gmail.com>
2025-10-24 17:19:49 +0530

    add my first file

---
+++ conditional statement/logical.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int x=3,y=4;
+    if (x>2 && y>3){
+    printf ("both condition are true");
+
+}
+
+else if(! (x>2 || y>4)) {
+    printf ("any one true");
+
+}
+
+
+}
